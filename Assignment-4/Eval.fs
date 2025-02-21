ff2e1018ab98250caba976cb99859141d3a5dd51
denw <denw@itu.dk>
2025-02-21 21:29:30 +0100

    File changes

--- Assignment-4/Eval.fs
+++ Assignment-4/Eval.fs
@@ -5,13 +5,23 @@ module Interpreter.Eval
     open Language
     open State
     
-    let rec arithEval (a: aexpr) (st: state) =
-        match a with
-        | Num int -> Num int
-        | Add(ex1, ex2) -> 
-        | Mul (ex1, ex2) ->
-        | Div (ex1, ex2) ->
-        | Mod (ex1, ex2) -> 
+    let rec arithEval (q: aexpr) (st: state) =
+        match q with
+        | Num int -> Some int
+        | Add(ex1, ex2) -> Option.bind (fun x ->
+                               Option.bind (fun y -> Some (x + y)) (arithEval ex2 st)
+                           ) (arithEval ex1 st)
+                           
+        | Mul (ex1, ex2) -> let a = arithEval ex1 st
+                            let b = arithEval ex2 st
+                            
+                            Option.bind(fun x ->
+                                Option.bind(fun y-> Some x * y
+                                    ) b
+                                ) a
+        | Div (ex1, ex2) -> None
+        
+        | Mod (ex1, ex2) -> None
         ;;
         
         
--- Assignment-4/obj/Debug/net7.0/Assignment4.AssemblyInfo.fs
+++ Assignment-4/obj/Debug/net7.0/Assignment4.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fafce07699c2b79bdcadad7b8721a8848c80ed1d")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1ee7019830d71ea8661f8d7ccd7852ffd06626")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("Assignment4")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
