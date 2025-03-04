b7fc3059190d3860142aa4b11b0043eb4654c5c0
Dennis <Denniswaweru@live.dk>
2025-03-04 14:41:29 +0100

    l

--- Assignment5/Assignment5/Eval.fs
+++ Assignment5/Assignment5/Eval.fs
@@ -4,28 +4,7 @@ module Interpreter.Eval
     open Result
     open Language
     open State
-    
-    let rec arithEval (a: aexpr) (st: state) =
-        match a with
-        | Num int -> Some int
-        | Var v -> st.map.TryFind v
-        | Add(ex1, ex2) ->  match (arithEval ex1 st , arithEval ex2 st) with
-                                | (Some exp1, Some exp2) -> Some (exp1 + exp2)
-                                | _ -> None  
-        | Mul (ex1, ex2) -> match (arithEval ex1 st , arithEval ex2 st) with
-                                | (Some exp1, Some exp2) -> Some (exp1 * exp2)
-                                | _ -> None         
-        | Div (ex1, ex2) -> match (arithEval ex1 st , arithEval ex2 st) with
-                                | (Some _, Some 0) -> None
-                                | (Some exp1, Some exp2)-> Some (exp1 / exp2)       
-                                | _ -> None
-        | Mod (ex1, ex2) -> match (arithEval ex1 st , arithEval ex2 st) with
-                                | (Some _, Some 0) -> None
-                                | (Some exp1, Some exp2)-> Some (exp1 % exp2)       
-                                | _ -> None
-    ;;
-        
-        
+       
     let rec arithEval2 (a: aexpr) (st: state) =
         match a with
         | Num int -> Some int
@@ -74,8 +53,8 @@ module Interpreter.Eval
                                ) b
                            )a
                         
-        | Lt (ex1, ex2) -> let a = arithEval ex1 st 
-                           let b = arithEval ex2 st
+        | Lt (ex1, ex2) -> let a = arithEval2 ex1 st 
+                           let b = arithEval2 ex2 st
                            
                            Option.bind (fun x ->
                                Option.bind(fun y ->  Some ((<) x y)
