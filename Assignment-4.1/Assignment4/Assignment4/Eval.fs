bdb7e8df1707ccff56eeacd7b8644aa0f28a80e6
denw <denw@itu.dk>
2025-03-09 19:29:25 +0100

    new

---
+++ Assignment-4.1/Assignment4/Assignment4/Eval.fs
@@ -0,0 +1,11 @@
+module Interpreter.Eval
+
+    open Result
+    open Language
+    open State
+    
+    let arithEval _ = failwith "not implemented"
+    let arithEval2 _ = failwith "not implemented"
+    
+    let boolEval _ = failwith "not implemented"
+    let stmntEval _ = failwith "not implemented"
